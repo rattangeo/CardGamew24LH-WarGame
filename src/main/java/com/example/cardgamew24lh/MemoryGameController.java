a7a0a24ddb849b50eccc7e1e90aef2599f0f7e8c
Rattan Ashat <rashat@lakeheadu.ca>
2024-01-29 15:37:59 -0500

    updated controller class

--- src/main/java/com/example/cardgamew24lh/MemoryGameController.java
+++ src/main/java/com/example/cardgamew24lh/MemoryGameController.java
@@ -2,10 +2,18 @@ package com.example.cardgamew24lh;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 
-public class MemoryGameController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+public class MemoryGameController implements Initializable {
 
     @FXML
     private FlowPane cardFlowPane;
@@ -16,8 +24,101 @@ public class MemoryGameController {
     @FXML
     private Label guessLabel;
 
+    private ArrayList<MemoryCard> cardsInGame;
+    private  MemoryCard firstCard, secondCard;
+    private int numOfGuess;
+    private int numOfMatches;
+
     @FXML
     void playAgain(ActionEvent event) {
+        firstCard = null;
+        secondCard = null;
+
+        DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+
+        cardsInGame = new ArrayList<>();
+
+        for (int i=0; i < cardFlowPane.getChildren().size()/2;i++){
+            Card cardDealt = deck.dealTopCard();
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(),cardDealt.getFaceName()));
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(),cardDealt.getFaceName()));
+        }
+        Collections.shuffle(cardsInGame);
+        flipAllCards();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    /**
+     * This will add a number to each imageView and set the image to the back of the card
+     */
+    private void initializeImageView(){
+        for (int i = 0; i<cardFlowPane.getChildren().size();i++) {
+            ImageView imageView = (ImageView) cardFlowPane.getChildren().get(i);
+            imageView.setImage(new Image(Card.class.getResourceAsStream("images/back_of_card.png")));
+            imageView.setUserData(i);
+            //register a click listener
+            imageView.setOnMouseClicked(event ->{
+                flipCard((int) imageView.getUserData());
+            });
+        }
+    }
+
+    /**
+     * This will show the back of all cards that are not matched
+     */
+    private void flipAllCards(){
+        for (int i=0; i<cardsInGame.size();i++){
+            ImageView imageView = (ImageView) cardFlowPane.getChildren().get(i);
+            MemoryCard card = cardsInGame.get(i);
+
+            if (card.isMatched())
+                imageView.setImage(card.getImage());
+            else
+                imageView.setImage(card.getBackOfCardImage());
+        }
+
+    }
+
+
+    private void flipCard(int indexOfCard){
+        if (firstCard == null && secondCard == null)
+            flipAllCards();
+
+        ImageView imageView = (ImageView) cardFlowPane.getChildren().get(indexOfCard);
+
+        if (firstCard == null){
+            firstCard = cardsInGame.get(indexOfCard);
+            imageView.setImage(firstCard.getImage());
+        } else if (secondCard == null) {
+            numOfGuess++;
+            secondCard = cardsInGame.get(indexOfCard);
+            imageView.setImage(secondCard.getImage());
+            checkForMatch();
+            updateLables();
+        }
+
+
+    }
+
+    private void updateLables(){
+        correctGuessLabel.setText(Integer.toString(numOfMatches));
+        guessLabel.setText(Integer.toString(numOfGuess));
+    }
+
+    private void checkForMatch(){
+        if (firstCard.isSameCard(secondCard)){
+            numOfMatches++;
+            firstCard.setMatched(true);
+            secondCard.setMatched(true);
+        }
+        firstCard = null;
+        secondCard = null;
 
     }
 
