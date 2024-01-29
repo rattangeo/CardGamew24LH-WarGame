5068630fd313653259604d57eb736806dab5ec27
Rattan Ashat <rashat@lakeheadu.ca>
2024-01-29 13:20:30 -0500

    added images

--- src/main/java/com/example/cardgamew24lh/Card.java
+++ src/main/java/com/example/cardgamew24lh/Card.java
@@ -1,2 +1,121 @@
-package com.example.cardgamew24lh;public class Card {
+package com.example.cardgamew24lh;
+
+import javafx.scene.image.Image;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Card {
+    private String suit;
+    private String faceName;
+
+    /**
+     * This is the constructor, it will create a new Card object in system
+     * memory.  It will validate the suit and faceName are found in standard
+     * playing cards
+     *
+     * @param suit     "hearts","clubs","spades","diamonds"
+     * @param faceName "2","3","4",..."jack","queen","king","ace"
+     */
+    public Card(String suit, String faceName) {
+        setSuit(suit);
+        setFaceName(faceName);
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * Validates the argument and sets the instance variable
+     *
+     * @param suit "hearts","clubs","spades","diamonds"
+     */
+    public void setSuit(String suit) {
+        suit = suit.toLowerCase();
+
+        //check for s in the final position of the string, if not add one
+        if (suit.charAt(suit.length() - 1) != 's')
+            suit = suit + "s";
+
+        List<String> validSuits = getValidSuits();
+
+        //test if the argument is in the list of valid suits
+        if (validSuits.contains(suit))
+            this.suit = suit;
+        else
+            throw new IllegalArgumentException(suit + " was not in the list of " +
+                    validSuits);
+    }
+
+    /**
+     * This method returns a list of all the valid suits
+     *
+     * @return
+     */
+    public static List<String> getValidSuits() {
+        return Arrays.asList("hearts", "clubs", "spades", "diamonds");
+    }
+
+    public String getFaceName() {
+        return faceName;
+    }
+
+    public static List<String> getFaceNames() {
+        return Arrays.asList("2", "3", "4",
+                "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace");
+    }
+
+    /**
+     * validates the argument and sets the instance variable
+     *
+     * @param faceName "2","3","4",..."jack","queen","king","ace"
+     */
+    public void setFaceName(String faceName) {
+        List<String> validFaceNames = getFaceNames();
+        faceName = faceName.toLowerCase();
+
+        if (validFaceNames.contains(faceName))
+            this.faceName = faceName;
+        else
+            throw new IllegalArgumentException(faceName + " must be from the list "
+                    + validFaceNames);
+    }
+
+    @Override
+    public String toString() {
+        return faceName + " of " + suit;
+    }
+
+    /**
+     * This method returns the colour of the suit.  Red = hearts or diamonds
+     * Black = clubs or spades
+     */
+    public String getColour() {
+        if (suit.equals("hearts") || suit.equals("diamonds"))
+            return "red";
+        else
+            return "black";
+    }
+
+    /**
+     * This returns the value of the card
+     * index    ->  0 , 1 , 2,  3 , 4 , 5 , 6,  7,  8,    9,    10,      11,   12
+     * faceName -> "2","3","4","5","6","7","8","9","10","jack","queen","king","ace"
+     * value    ->  2 , 3,  4,  5,  6,  7,  8, 9,  10,   11,    12,     13,    14
+     */
+    public int getCardValue() {
+        List<String> faceNames = getFaceNames();
+        int indexPosition = faceNames.indexOf(faceName);
+        return indexPosition + 2;
+    }
+
+    /**
+     * This method returns an Image object to represent the card
+     */
+    public Image getImage() {
+        String imageName = "images/" + faceName + "_of_" + suit + ".png";
+        return new Image(Card.class.getResourceAsStream(imageName));
+    }
 }
--- src/main/java/com/example/cardgamew24lh/DeckOfCards.java
+++ src/main/java/com/example/cardgamew24lh/DeckOfCards.java
@@ -1,2 +1,59 @@
-package com.example.cardgamew24lh;public class DeckOfCards {
+package com.example.cardgamew24lh;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DeckOfCards {
+    //This is the instance variable that holds all of the Card objects.
+    //You can think of it sort of like the box that holds all the Card's
+    private ArrayList<Card> deck;
+
+    /**
+     * This is the constructor.  It will allocate system memory for the
+     * ArrayList to hold Card objects.  It will also create 52 Card objects
+     * and put them in the ArrayList
+     */
+    public DeckOfCards()
+    {
+        deck = new ArrayList<>();
+        //"hearts","clubs","spades","diamonds"
+        //   0         1       2        3
+        List<String> suits = Card.getValidSuits();
+        List<String> faceNames = Card.getFaceNames();
+
+        for (int i=0 ; i< suits.size(); i++)
+        {
+            for (int x=0; x <faceNames.size() ; x++){
+                deck.add(new Card(suits.get(i),faceNames.get(x)));
+            }
+        }
+    }
+
+    /**
+     * This method returns the top Card from the deck or null if the
+     * deck is empty
+     */
+    public Card dealTopCard()
+    {
+        if (deck.size()>0)
+            return deck.remove(0);
+        return null;
+    }
+
+    /**
+     * This method returns the number of card objects remaining in the deck
+     */
+    public int getNumOfCards()
+    {
+        return deck.size();
+    }
+
+    /**
+     * This method will shuffle the deck (randomize the Card order)
+     */
+    public void shuffle()
+    {
+        Collections.shuffle(deck);
+    }
 }
