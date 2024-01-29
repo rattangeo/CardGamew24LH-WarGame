57c4c1c89788077f1c40fd57eb1895ddad322c83
Rattan Ashat <rashat@lakeheadu.ca>
2024-01-29 14:20:03 -0500

    updated view & controller class

--- src/main/java/com/example/cardgamew24lh/MemoryGameController.java
+++ src/main/java/com/example/cardgamew24lh/MemoryGameController.java
@@ -1,4 +1,24 @@
 package com.example.cardgamew24lh;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+
 public class MemoryGameController {
+
+    @FXML
+    private FlowPane cardFlowPane;
+
+    @FXML
+    private Label correctGuessLabel;
+
+    @FXML
+    private Label guessLabel;
+
+    @FXML
+    void playAgain(ActionEvent event) {
+
+    }
+
 }
